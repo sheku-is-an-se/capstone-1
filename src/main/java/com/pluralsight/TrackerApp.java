299914c707a970698c9f69607d51bd717476b675
sheku-is-a-se <sheku2026@gmail.com>
2026-04-27 13:52:54 -0400

    Create a method for visualizing report and finish setting up visuals

--- src/main/java/com/pluralsight/TrackerApp.java
+++ src/main/java/com/pluralsight/TrackerApp.java
@@ -1,19 +1,25 @@
 
 package com.pluralsight;
 
-import java.util.Locale;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 
 public class TrackerApp {
     static Scanner scanner = new Scanner(System.in);
-
+    ArrayList<Transactions> transactions = new ArrayList<>();
+    public static final String TRANSACTIONS_FILE_NAME = "src/main/resources/products.csv";
 
 
     public static void main(String[] args) {
         mainMenu();
         System.out.println("Thank you, come back soon!");
+
+        FileReader fileReader = new FileReader()
+
+
     }
 
 
@@ -69,22 +75,21 @@ public class TrackerApp {
     private static void ledgerMenu() {
         String prompt =
                 """
-                        ========================================
-                                        Ledger
-                        ========================================
-                       \s
-                        Please choose an option:
-                       \s
-                        A) All
-                        D) Deposits
-                        P) Payments
-                        R) Reports
-                        H) Home
-                       \s
-                        Enter your choice:\s""";
+                         ========================================
+                                         Ledger
+                         ========================================
+                        \s
+                         Please choose an option:
+                        \s
+                         A) All
+                         D) Deposits
+                         P) Payments
+                         R) Reports
+                         H) Home
+                        \s
+                         Enter your choice:\s""";
 
         boolean running = true;
-
 
 
         do {
@@ -103,8 +108,8 @@ public class TrackerApp {
                     running = false;
                     break;
                 case "R":
-                    System.out.println("Reports.");
                     running = false;
+                    showReportMenu();
                     break;
                 case "H":
                     running = false;
@@ -115,6 +120,59 @@ public class TrackerApp {
                     break;
             }
         } while (running);
+
+    }
+
+        private static void showReportMenu() {
+            String prompt =
+                    """
+                            ========================================
+                                            Reports
+                            ========================================
+                           \s
+                            Please choose an option:
+                           \s
+                            1) All
+                            2) Deposits
+                            3) Payments
+                            4) Reports
+                            5) Home
+                            0) Back
+                           \s
+                            Enter your choice:\s""";
+
+            boolean running = true;
+
+
+
+            do {
+                System.out.println(prompt);
+                String userMenu = scanner.nextLine();
+
+                switch (userMenu) {
+                    case "A":
+                        //DisplayProduct();
+                        break;
+                    case "D":
+                        System.out.println("Deposits");
+                        break;
+                    case "P":
+                        System.out.println("Payments.");
+                        running = false;
+                        break;
+                    case "R":
+                        running = false;
+                        showReports();
+                        break;
+                    case "H":
+                        running = false;
+                        mainMenu();
+                        break;
+                    default:
+                        System.err.println(("Oops! That wasn't a valid option."));
+                        break;
+                }
+            } while (running);
 
 
 
